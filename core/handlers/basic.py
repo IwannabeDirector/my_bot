9ee9b0bbc6e1e5055cf2ca19b0162a78037800ce
IwannabeDirector <a.nikitin1@g.nsu.ru>
2023-12-10 20:41:03 +0700

    init

---
+++ core/handlers/basic.py
@@ -0,0 +1,8 @@
+from aiogram import Bot
+from aiogram.types import Message
+
+
+async def get_start(message: Message, bot: Bot):
+	await bot.send_message(
+		message.from_user.id, f'<b>Добро пожаловать на сервер шизофрения, {message.from_user.first_name}</b>'
+	)
---
+++ core/main.py
@@ -0,0 +1,37 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, ContentType
+from core.handlers.basic import get_start
+import asyncio
+import logging
+
+from core.settings import settings
+from aiogram.filters import Command
+
+
+async def start_bot(bot: Bot):
+	await bot.send_message(settings.bots.admin_id, text='Bot started')
+
+
+async def stop_bot(bot: Bot):
+	await bot.send_message(settings.bots.admin_id, text='Bot stopped')
+
+
+async def start():
+	logging.basicConfig(level=logging.INFO,
+						format='%(asctime)s - [%(levelname)s] - %(name)s - '
+								'(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
+						)
+	bot = Bot(token=settings.bots.bot_token, parse_mode='HTML')
+
+	dp = Dispatcher()
+	dp.startup.register(start_bot)
+	dp.shutdown.register(stop_bot)
+	dp.message.register(get_start, Command(commands=['start', 'run']))
+	try:
+		await dp.start_polling(bot)
+	finally:
+		await bot.session.close()
+
+
+if __name__ == '__main__':
+	asyncio.run(start())
